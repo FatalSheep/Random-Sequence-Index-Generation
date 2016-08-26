c61c3b0e1dad43b81add598645194eeb45523cf6
Alexander Griffis <fatalsleep@gmail.com>
2016-08-26 15:09:44 -0700

    Rand() calls via function/member function pointers
    Previously we used rand(), now you can pass a function pointer or function member pointer to use your own random functions.

--- rand_indices.h
+++ rand_indices.h
@@ -1,19 +1,27 @@
 #ifndef __RAND_INDICES__
 #define __RAND_INDICES__
-  #include <stdlib.h>
-  #include <string.h>
-  
-  int* rand_indicies(int n) {
-      int* ptr = (int*) malloc(n * sizeof(int));
-  
-      for (int i = 0, j; i < n; i++) {
-          j = (i == 0) ? 0 : rand() % (i + 1);
-  
-          if (j != i)
-              ptr[i] = ptr[j];
-          ptr[j] = i;
-      }
-  
-      return ptr;
-  }
+    #include <stdlib.h>
+    #include <string.h>
+
+    template<int S, typename T>
+    void rand_indicies(int(&ptr)[S], __int8 (T::*func)(), T* object) {
+        for (int i = 0, j; i < S; i++) {
+            j = (i == 0) ? 0 : (static_cast<int>((object->*func)()) % (i + 1));
+
+            if (j != i)
+                ptr[i] = ptr[j];
+            ptr[j] = i;
+        }
+    }
+
+    template<int S>
+    void rand_indicies(int(&ptr)[S], __int8 (*func)()) {
+        for (int i = 0, j; i < S; i++) {
+            j = (i == 0) ? 0 : (static_cast<int>((*func)()) % (i + 1));
+
+            if (j != i)
+                ptr[i] = ptr[j];
+            ptr[j] = i;
+        }
+    }
 #endif
